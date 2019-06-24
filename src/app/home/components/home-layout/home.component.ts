9b4cd02421e66ae15df22f25ccfddba7bb99d783
Miguel López <miguelopezv@gmail.com>
2019-06-24 11:44:54 -0500

    retrieve data from Spotify API and home layout

--- src/app/home/components/home-layout/home.component.ts
+++ src/app/home/components/home-layout/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class HomeComponent {
+  newReleases: any[] = [];
+  constructor(private spotifyService: SpotifyService) {
+    this.spotifyService
+      .getNewReleases()
+      .subscribe((data: any) => (this.newReleases = data.albums.items));
   }
-
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import * as fromComponents from './components';
+import { SpotifyService } from '../services/spotify.service';
+import { HttpClientModule } from '@angular/common/http';
 
 const ROUTES: Routes = [{ path: '', component: fromComponents.HomeComponent }];
 
 @NgModule({
   declarations: [...fromComponents.components],
-  imports: [CommonModule, RouterModule.forChild(ROUTES)]
+  imports: [CommonModule, HttpClientModule, RouterModule.forChild(ROUTES)],
+  providers: [SpotifyService]
 })
 export class HomeModule {}
