85e6a2b84dd9a12b089825562c17093932ed299f
Miguel López <miguelopezv@gmail.com>
2019-06-24 00:54:09 -0500

    update navbar

--- src/app/shared/navbar/navbar.module.ts
+++ src/app/shared/navbar/navbar.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { RouterModule } from '@angular/router';
 import * as fromComponents from './components';
 
 @NgModule({
   declarations: fromComponents.navbarComponents,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: fromComponents.navbarComponents,
   entryComponents: fromComponents.navbarComponents
 })
