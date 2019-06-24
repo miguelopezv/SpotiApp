fd11dde46c2f63b81598be4d0475b57c0e88e7e4
Miguel López <miguelopezv@gmail.com>
2019-06-24 17:51:36 -0500

    navigateTo artist page

--- src/app/shared/card/components/card/card.component.ts
+++ src/app/shared/card/components/card/card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
@@ -7,4 +7,18 @@ import { Component, Input } from '@angular/core';
 })
 export class CardComponent {
   @Input() item: any;
+
+  constructor(private router: Router) {}
+
+  goToArtist(item: any) {
+    let artistId: string;
+
+    if (item.type === 'artist') {
+      artistId = item.id;
+    } else {
+      artistId = item.artists[0].id;
+    }
+
+    this.router.navigate(['/artist', artistId]);
+  }
 }
