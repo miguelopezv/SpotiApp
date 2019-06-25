29470d49fe97be4332b27832ef793919335cc871
Miguel López <miguelopezv@gmail.com>
2019-06-24 19:20:26 -0500

    display top tracks

--- src/app/artist/components/artist-layout/artist.component.ts
+++ src/app/artist/components/artist-layout/artist.component.ts
@@ -9,16 +9,26 @@ import { SpotifyService } from 'src/app/services/spotify.service';
 })
 export class ArtistComponent {
   artist: any;
+  topTracks: any[];
   constructor(
     private router: ActivatedRoute,
     private spotifyService: SpotifyService
   ) {
-    this.router.params.subscribe(params => this.getArtist(params.id));
+    this.router.params.subscribe(params => {
+      this.getArtist(params.id);
+      this.getTopTracks(params.id);
+    });
   }
 
   getArtist(id: string) {
     this.spotifyService
       .getArtist(id)
       .subscribe(artist => (this.artist = artist));
+  }
+
+  getTopTracks(id: string) {
+    this.spotifyService
+      .getTopTracks(id)
+      .subscribe(topTracks => (this.topTracks = topTracks));
   }
 }
