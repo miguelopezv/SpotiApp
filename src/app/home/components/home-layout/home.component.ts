889dfc1140be151cbd4d55874d782cc4bb8ff9f4
Miguel López <miguellopez@vitalware.com>
2019-06-25 21:58:09 -0500

    get token when app init

--- src/app/home/components/home-layout/home.component.ts
+++ src/app/home/components/home-layout/home.component.ts
@@ -11,6 +11,18 @@ export class HomeComponent {
   error: any;
 
   constructor(private spotifyService: SpotifyService) {
+    if (!sessionStorage.getItem('token')) {
+      this.getToken();
+    } else {
+      this.getNewReleases();
+    }
+  }
+
+  getToken() {
+    this.spotifyService.getToken().subscribe(data => this.getNewReleases());
+  }
+
+  getNewReleases() {
     this.spotifyService
       .getNewReleases()
       .subscribe(
